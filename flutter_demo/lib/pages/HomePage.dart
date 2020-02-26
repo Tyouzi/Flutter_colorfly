4993b2fd4f2620a760c2bc7f0ad707cc673923d5
刘成 <liucheng@joycastle.mobi>
2020-02-26 14:13:37 +0800

    init

---
+++ flutter_demo/lib/pages/HomePage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+
+class HomePage extends StatefulWidget{
+  @override
+  State<HomePage> createState() {
+    // TODO: implement createState
+    return  HomePageState();
+  }
+
+}
+class HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Container(
+        child: new Center(
+          child:Text("1234")
+        )
+    );
+  }
+
+}
---
+++ flutter_demo/lib/pages/ShowRoom.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+
+class ShowRoom extends StatefulWidget{
+  @override
+  State<ShowRoom> createState() {
+    // TODO: implement createState
+    return  ShowRoomPageState();
+  }
+
+}
+class ShowRoomPageState extends State<ShowRoom> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Container(
+        child: new Center(
+            child:Text("22222")
+        )
+    );
+  }
+
+}
